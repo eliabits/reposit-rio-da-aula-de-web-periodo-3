0889acb3dc9ebf14700052ce6641864862f3a451
Eliabe Rafael Oliveira Silva <eliab1rafael@gmail.com>
2026-05-21 20:47:21 -0300

    Meu Readme.md

---
+++ .vscode/a = [1,2,3,5].py
@@ -0,0 +1,6 @@
+a = [1,2,3,5]
+copia = a
+a.append(20)
+
+print(a)
+print(copia)
---
+++ aula 1.js
@@ -0,0 +1,37 @@
+'-----------------------------------------------------------------'
+function soma(a, b) {
+    return a + b;
+}
+const somar = (a, b) => a+b;
+const dobrar = (a) => a * 2;
+const quadrado = a => a*a;
+'-----------------------------------------------------------------'
+function conectar(host = 'localhost', porta = 3000){
+    return `${host}:${porta}`;
+}
+console.log(conectar());
+console.log(conectar('127.0.0.1'))
+console.log(conectar('127.0.0.1', 8080))
+'-----------------------------------------------------------------'
+function mostrarTudo(...args){
+    console.log('recebi: ', args);}
+mostrarTudo("é o helio", "eliabe", "cachumba");
+'-----------------------------------------------------------------'
+function somartudo(...numeros){
+    console.log(numeros)
+    return numeros.reduce((total, n)=> total+n, 0)
+}
+console.log(somartudo(52,152,51));
+'-----------------------------------------------------------------'
+function listar(primeiro, ...outros){
+    console.log('primeiro: ', primeiro)
+    console.log('outros: ', outros)
+}
+listar('helio', 'eliabe', 'catatonico');
+
+const original = [10,20,30];
+const copia = original;
+original.push(50);
+console.log(original)
+console.log(copia)
+typeof null
